13c6f9dce6b7b2f6acb99a8e23fa66f457baf8d0
Muhammad Hassan <60579417+hassan1344@users.noreply.github.com>
2022-01-16 22:15:39 +0500

    setting attributes / rendering classes dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1>;
-    }
+  state = {
+    count: 1,
+    //imageUrl: "https://picsum.photos/200", //generates a random photo of 200 pixels
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-primary">Increment</button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "alert alert-";
+    classes += this.state.count === 0 ? "success" : "danger";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
 }
- 
+
 export default Counter;
