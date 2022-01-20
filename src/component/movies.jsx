3e88433813aaad6a0cd75bce30757c6159a8d9f7
Muhammad Hassan <60579417+hassan1344@users.noreply.github.com>
2022-01-20 20:15:10 +0500

    Deletion of movie / conditional rendering

--- src/component/movies.jsx
+++ src/component/movies.jsx
@@ -6,26 +6,44 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
+  handleDelete = (movie) => {
+    const moviesTemp = this.state.movies.filter((m) => m._id != movie._id);
+    this.setState({ movies: moviesTemp });
+  };
+
   render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>There are no movies in the Database!</p>;
+
     return (
       <div>
         <h2>Movies Component</h2>
-        <table className="table table-dark">
+        <p>Showing {count} movies in the Database</p>
+        <table className="table">
           <thead>
             <tr>
               <th>Title</th>
               <th>Genre</th>
               <th>Stock</th>
               <th>Rate</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
             {this.state.movies.map((movie) => (
-              <tr>
+              <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
